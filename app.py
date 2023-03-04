84e349b8e768cd186319395a39a5c96cafa46e72
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2023-03-04 21:21:42 +0800

    allow cors origin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.api import config_blueprint
 from app.database import config_database
 from app import config_extensions
@@ -6,10 +7,13 @@ from app import config_extensions
 
 def create_app():
     flask_app = Flask(__name__)
-    flask_app.config.from_object('config_dev')  # config config_dev config_prod
+    #  load config file
+    flask_app.config.from_object('config_dev')
     config_extensions(flask_app)
     config_database(flask_app)
     config_blueprint(flask_app)
+    #  allow cors origin
+    CORS(flask_app, supports_credentials=True)
     return flask_app
 
 
